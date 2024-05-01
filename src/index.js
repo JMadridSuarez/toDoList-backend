e9fb56cd030aa6013f3d53f54f6f0d5afc46073d
Jose Madrid <mikemadrid96@gmail.com>
2024-05-01 11:15:20 -0500

    db config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const todoappRoutes = require('./V1/routes/todoappRoutes');
 const app = express();
 const PORT = process.env.DB_PORT ?? 8000;
+const pool = require('./database/db')
 
 //app listen
 app.listen(PORT, ()=>{
@@ -15,3 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1",todoappRoutes);
 //
+
+app.get('/ping', async(req,res)=>{
+    const time = await pool.query('SELECT NOW();')
+    res.json({
+        "pong": time.rows[0]
+    })
+})
