aaafdb60fd9f69ad835441d004a996b28280a25b
Jose Madrid <mikemadrid96@gmail.com>
2024-04-30 21:18:35 -0500

    alv

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,7 @@ const Pool = require('pg').Pool
 require('dotenv').config();
 
 const pool = new Pool({
+    connectionString:process.env.DATABASE_URL,
     user: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
