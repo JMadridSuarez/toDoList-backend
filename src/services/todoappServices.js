886da2115454705843d48de0af74383b6250442e
Jose Madrid <mikemadrid96@gmail.com>
2024-05-01 12:17:39 -0500

    alv

--- src/services/todoappServices.js
+++ src/services/todoappServices.js
@@ -48,8 +48,9 @@ const signUp = async(req,res)=>{
         await pool.query(`INSERT INTO users (email,hashed_password) VALUES($1,$2)`,
             [email, hashedPassword]);
            
-        const token = jwt.sign({email}, 'secret', {expiresIn:'1hr'});
-        res.json({email, token});
+        //const token = jwt.sign({email}, 'secret', {expiresIn:'1hr'});
+        //await res.json({email, token});
+        return('success')
     } catch (error) {
         console.error(error)
         if(error){
